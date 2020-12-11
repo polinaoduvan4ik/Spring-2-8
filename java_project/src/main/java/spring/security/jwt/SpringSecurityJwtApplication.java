4bdb15ea45f9481156e5a9f093f65d3f479e9ec3
Polina <polinaoduvan4ik@mail.ru>
2020-12-11 05:24:03 +0300

    final version

--- java_project/src/main/java/spring/security/jwt/SpringSecurityJwtApplication.java
+++ java_project/src/main/java/spring/security/jwt/SpringSecurityJwtApplication.java
@@ -1,14 +1,12 @@
-package com.example.project;
-
+package spring.security.jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServingWebContentApplication {
+public class SpringSecurityJwtApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServingWebContentApplication.class, args);
+        SpringApplication.run(SpringSecurityJwtApplication.class, args);
     }
-
 }
